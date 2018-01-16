eda8837dd7f089a97566c1194574fbdaa3842b8b
Sharmina Akhtar <sakht318@gmail.com>
2018-01-16 13:18:59 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,22 +42,15 @@ class Owner
   end
 
   def walk_dogs
-    walk_dog = happy
-    if dog = walk_dog
-       Dog.mood == "happy"
-    else
-      nil
-    end
+    pets[:dogs].each {|dog| dog.mood = "happy"}
   end
 
   def play_with_cats
-
-
-
+    pets[:cats].each{|cat| cat.mood = "happy"}
   end
 
   def feed_fish
-
+    pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
   def sell_pets
